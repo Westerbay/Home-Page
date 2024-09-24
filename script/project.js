714e1e447e7250daa099d11a4b91d0848acb7f30
Wester <mathis.dubuisson@ecole.ensicaen.fr>
2024-09-24 21:28:14 +0200

    Premiere Version

--- script/project.js
+++ script/project.js
@@ -1,7 +1,8 @@
-var backgroundColor = "darkblue";
-var hoverColor = "blue";
+var backgroundColor = "rgb(40, 40, 40)";
+var hoverColor = "grey";
 
-var projectContent = document.getElementById("projectContent");
+var container = document.getElementById("container");
+var containerWidth = 39;
 
 var projectEyefox = document.getElementById("project1");
 var projectBattle = document.getElementById("project2");
@@ -13,123 +14,96 @@ var projectGDC = document.getElementById("project7");
 var projectNation = document.getElementById("project8");
 var selectedProject = projectEyefox;
 
+var corrButton = new Map();
+corrButton.set(projectEyefox, 0);
+corrButton.set(projectBattle, 1);
+corrButton.set(projectMinidex, 2);
+corrButton.set(projectAddsUp, 3);
+corrButton.set(projectLindenmayer, 4);
+corrButton.set(projectTaquinSudoku, 5);
+corrButton.set(projectGDC, 6);
+corrButton.set(projectNation, 7);
+
+function main() {
+	selectEyefox();
+	for (var element of document.getElementsByTagName("article")) {
+		element.style.transform = "translateX(0)";
+		element.style.opacity = 100;
+	}
+}
+
 function selectEyefox() {
 	selectedProject.style.background = backgroundColor;
 	projectEyefox.style.background = hoverColor;
 	selectedProject = projectEyefox;
-	projectContent.innerHTML = `
-				<img src='img/logo/Python.png' alt='python'>
-				<h2>Eyefox Puzzle</h2>
-				<h3>Personnal Project</h3>
-				<p>
-					This game is a python project where you have to reproduce the left's grid by modifying the right's grid. How does it work?  Tap a square to swap its face and its neighbors' face. Be careful, the difficulty is considerably growing up every 30 levels. 
-				</p>
-				<img src='img/project/puzzle.png' alt='Eyefox Puzzle'>
-	`;
+	container.style.transform = "translateX(0)";
 }
 
 function selectBattle() {
 	selectedProject.style.background = backgroundColor;
 	projectBattle.style.background = hoverColor;
 	selectedProject = projectBattle;
-	projectContent.innerHTML = `
-				<img src='img/logo/Python.png' alt='python'>
-				<h2>Card Game</h2>
-				<h3>Personnal Project</h3>
-				<p>
-					This game is a python project where you have to reproduce the left's grid by modifying the right's grid. How does it work?  Tap a square to swap its face and its neighbors' face. Be careful, the difficulty is considerably growing up every 30 levels. 
-				</p>
-				<img src='img/project/battle.png' alt='Eyefox Puzzle'>
-	`;
+	container.style.transform = "translateX(-" + containerWidth + "em)";
 }
 
 function selectMinidex() {
 	selectedProject.style.background = backgroundColor;
 	projectMinidex.style.background = hoverColor;
 	selectedProject = projectMinidex;
-	projectContent.innerHTML = `
-				<img src='img/logo/Python.png' alt='python'>
-				<h2>Minidex [FR]</h2>
-				<h3>Personnal Project</h3>
-				<p>
-					This game is a python project where you have to reproduce the left's grid by modifying the right's grid. How does it work?  Tap a square to swap its face and its neighbors' face. Be careful, the difficulty is considerably growing up every 30 levels. 
-				</p>
-				<img src='img/project/minidex.png' alt='Eyefox Puzzle'>
-	`;
+	container.style.transform = "translateX(-" + containerWidth*2 + "em)";
 }
 
 function selectAddsUp() {
 	selectedProject.style.background = backgroundColor;
 	projectAddsUp.style.background = hoverColor;
 	selectedProject = projectAddsUp;
-	projectContent.innerHTML = `
-				<img src='img/logo/Python.png' alt='python'>
-				<h2>It Adds Up</h2>
-				<h3>Personnal Project</h3>
-				<p>
-					This game is a python project where you have to reproduce the left's grid by modifying the right's grid. How does it work?  Tap a square to swap its face and its neighbors' face. Be careful, the difficulty is considerably growing up every 30 levels. 
-				</p>
-				<img src='img/project/AddsUp.png' alt='Eyefox Puzzle'>
-	`;
+	container.style.transform = "translateX(-" + containerWidth*3 + "em)";
 }
 
 function selectLindenmayer() {
 	selectedProject.style.background = backgroundColor;
 	projectLindenmayer.style.background = hoverColor;
 	selectedProject = projectLindenmayer;
-	projectContent.innerHTML = `
-				<img src='img/logo/Java.png' alt='Java'>
-				<h2>L-Systems</h2>
-				<h3>School Project</h3>
-				<p>
-					This game is a python project where you have to reproduce the left's grid by modifying the right's grid. How does it work?  Tap a square to swap its face and its neighbors' face. Be careful, the difficulty is considerably growing up every 30 levels. 
-				</p>
-				<img src='img/project/Lindenmayer.png' alt='Eyefox Puzzle'>
-	`;
+	container.style.transform = "translateX(-" + containerWidth*4 + "em)";
 }
 
 function selectTaquinSudoku() {
 	selectedProject.style.background = backgroundColor;
 	projectTaquinSudoku.style.background = hoverColor;
 	selectedProject = projectTaquinSudoku;
-	projectContent.innerHTML = `
-				<img src='img/logo/Python.png' alt='python'>
-				<h2>Taquin's Sudoku</h2>
-				<h3>Personnal Project</h3>
-				<p>
-					This game is a python project where you have to reproduce the left's grid by modifying the right's grid. How does it work?  Tap a square to swap its face and its neighbors' face. Be careful, the difficulty is considerably growing up every 30 levels. 
-				</p>
-				<img src='img/project/TaquinSudoku.png' alt='Eyefox Puzzle'>
-	`;
+	container.style.transform = "translateX(-" + containerWidth*5 + "em)";
 }
 
 function selectGDC() {
 	selectedProject.style.background = backgroundColor;
 	projectGDC.style.background = hoverColor;
 	selectedProject = projectGDC;
-	projectContent.innerHTML = `
-				<img src='img/logo/C.png' alt='C'>
-				<h2>Race Event</h2>
-				<h3>School Project</h3>
-				<p>
-					This game is a python project where you have to reproduce the left's grid by modifying the right's grid. How does it work?  Tap a square to swap its face and its neighbors' face. Be careful, the difficulty is considerably growing up every 30 levels. 
-				</p>
-				<img src='img/project/GDC.png' alt='Eyefox Puzzle'>
-	`;
+	container.style.transform = "translateX(-" + containerWidth*6 + "em)";
 }
 
 function selectNation() {
 	selectedProject.style.background = backgroundColor;
 	projectNation.style.background = hoverColor;
 	selectedProject = projectNation;
-	projectContent.innerHTML = `
-				<img src='img/logo/Java.png' alt='java'>
-				<h2>Nation's Waltz</h2>
-				<h3>Personnal Project</h3>
-				<p>
-					This game is a python project where you have to reproduce the left's grid by modifying the right's grid. How does it work?  Tap a square to swap its face and its neighbors' face. Be careful, the difficulty is considerably growing up every 30 levels. 
-				</p>
-				<img src='img/project/nation.png' alt='Eyefox Puzzle'>
-	`;
+	container.style.transform = "translateX(-" + containerWidth*7 + "em)";
+}
+
+var tabFunction = [
+	selectEyefox, 
+	selectBattle,
+	selectMinidex,
+	selectAddsUp,	
+	selectLindenmayer,
+	selectTaquinSudoku,
+	selectGDC,
+	selectNation
+];
+
+function buttonRight() {
+	tabFunction[(corrButton.get(selectedProject) + 1) % 8]();
+}
+
+function buttonLeft() {
+	tabFunction[(corrButton.get(selectedProject) - 1) % 8]();
 }
 
